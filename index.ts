f5decf2dfa21bc024a89a883d59387d97bbbee87
otarza <oto.zakalashvili@gmail.com>
2020-10-07 21:41:34 +0400

    1 file modified

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ import moment from 'moment';
 console.log("Hello world");
 
 document.getElementById('time').innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+console.log("Hello Stackblitz");
